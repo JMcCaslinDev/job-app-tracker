30a1e1be54dfae5fe5a7822a741dad71c0a8286e
jmccaslin21 <80499963+JMcCaslinDev@users.noreply.github.com>
2024-04-23 11:23:55 -0700

    Fix attempt for react build path issues

--- server/index.js
+++ server/index.js
@@ -5,10 +5,10 @@ const app = express();
 
 // Serve the React app in production
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+  app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+    res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html'));
   });
 }
 
