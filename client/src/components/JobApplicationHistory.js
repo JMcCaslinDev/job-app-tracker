e306b2eb1c3d2cde093d8e3a6e57e028a853cf66
jmccaslin21 <80499963+JMcCaslinDev@users.noreply.github.com>
2024-04-25 14:53:52 -0700

    Updated job application history axed action items

--- client/src/components/JobApplicationHistory.js
+++ client/src/components/JobApplicationHistory.js
@@ -15,26 +15,6 @@ const JobApplicationHistory = () => {
 
   return (
     <div className="job-application-history">
-      <div className="url-bar">
-        <input
-          type="text"
-          className="search-input"
-          placeholder="https://"
-          readOnly
-        />
-        <button className="search-button">Search</button>
-        <button className="add-button">Add Manual</button>
-      </div>
-      <div className="search-section">
-        <input
-          type="text"
-          placeholder="Search..."
-          value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
-        />
-        <button onClick={handleSearch}>Search</button>
-        <button onClick={handleAddManual}>Add Manual</button>
-      </div>
       <div className="history-section">
         {/* Display job application history */}
       </div>
