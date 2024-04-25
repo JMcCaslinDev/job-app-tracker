5de522f2ac8f61d8b77628c8ff0006245fa40742
jmccaslin21 <80499963+JMcCaslinDev@users.noreply.github.com>
2024-04-25 13:33:02 -0700

    Updated dashboard.js

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,6 +1,8 @@
+
+
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import jwtDecode from 'jwt-decode'; 
+import { jwtDecode } from 'jwt-decode';
 import axios from 'axios';
 import Navbar from './Navbar';
 import JobApplicationHistory from './JobApplicationHistory';
