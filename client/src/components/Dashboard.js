2b5eb5e80b88df6d9d1bd718cc8b19029292d898
jmccaslin21 <80499963+JMcCaslinDev@users.noreply.github.com>
2024-04-25 09:59:37 -0700

    Updated import statements

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,7 +1,7 @@
 // Dashboard.js
 import React from 'react';
-import Navbar from './components/Navbar';
-import JobApplicationHistory from './components/JobApplicationHistory';
+import Navbar from './Navbar';
+import JobApplicationHistory from './JobApplicationHistory';
 import '../css/Dashboard.css';
 
 const Dashboard = () => {
