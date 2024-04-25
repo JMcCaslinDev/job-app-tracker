59d3a06ffb7afb9e70309d351890a653427b7e5f
jmccaslin21 <80499963+JMcCaslinDev@users.noreply.github.com>
2024-04-25 11:21:35 -0700

    Fixed auth and added local testing support

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import jwtDecode from 'jwt-decode'; 
+import { jwtDecode } from 'jwt-decode';
 import Navbar from './Navbar';
 import JobApplicationHistory from './JobApplicationHistory';
 import '../css/Dashboard.css';
--- server/index.js
+++ server/index.js
@@ -157,7 +157,7 @@ app.delete('/api/job-applications/:id', verifyJwtToken, async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000 || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
