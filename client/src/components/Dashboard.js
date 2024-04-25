01bef08f3f1f0cfce6ab38771c29623867aa67ee
jmccaslin21 <80499963+JMcCaslinDev@users.noreply.github.com>
2024-04-25 11:07:27 -0700

    Trying a different decode library

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { jwt_decode as jwtDecode } from 'jwt-decode';
+import jwt from 'jsonwebtoken'; 
 import Navbar from './Navbar';
 import JobApplicationHistory from './JobApplicationHistory';
 import '../css/Dashboard.css';
@@ -18,18 +18,15 @@ const Dashboard = () => {
     }
 
     try {
-      // Decode the token and check for expiration
-      const decodedToken = jwtDecode(token);
-      
+      // Decode the token and check for expiration using jsonwebtoken
+      const decodedToken = jwt.decode(token); // Use jwt.decode here
+
       // Check if the token has expired
       const currentTime = Date.now() / 1000;
       if (decodedToken.exp < currentTime) {
         localStorage.removeItem('token');
         navigate('/login');
       }
-
-      // You might want to dispatch the decoded token to a global state
-      // or continue with additional checks if needed
 
     } catch (error) {
       // If token is invalid or can't be decoded, navigate to login
