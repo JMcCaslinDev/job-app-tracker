4d27de42095668fe3e433700df4b541506fd4c23
jmccaslin21 <80499963+JMcCaslinDev@users.noreply.github.com>
2024-04-25 18:08:23 -0700

    updated navbar flexboxes

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,21 +1,20 @@
 // components/Navbar.js
 import React from 'react';
 import NavbarButton from './NavbarButton';
-import '../css/Navbar.css'; // Update the path to your Navbar CSS if different
+import '../css/Navbar.css';
 
 const Navbar = () => {
-  // Add your navigation logic or handlers here
-
   const handleLogout = () => {
     // Logout logic here
   };
 
   return (
     <nav className="navbar">
-      <NavbarButton text="Dashboard" className="dashboard" />
-      {/* Add more buttons here */}
-      <NavbarButton text="Settings" className="settings" />
-      {/* More buttons can be added here as you create new pages */}
+      <div className="navbar-buttons">
+        <NavbarButton text="Dashboard" className="dashboard" />
+        <NavbarButton text="Settings" className="settings" />
+      </div>
+      <div className="navbar-spacer"></div>
       <NavbarButton text="Logout" className="logout" onClick={handleLogout} />
     </nav>
   );
