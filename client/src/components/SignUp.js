9092f003cabb62f74d9cd2b3209ec39c985cc7b9
jmccaslin21 <80499963+JMcCaslinDev@users.noreply.github.com>
2024-04-26 15:34:14 -0700

    updated imports to add useEffect at signup & login

--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import '../css/signUp.css';
