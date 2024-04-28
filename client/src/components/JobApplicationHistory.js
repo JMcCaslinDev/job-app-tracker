c17351214074443b1da32fb1b2314193f1833605
jmccaslin21 <80499963+JMcCaslinDev@users.noreply.github.com>
2024-04-27 18:04:39 -0700

    Should fix import issues with naming

--- client/src/components/JobApplicationHistory.js
+++ client/src/components/JobApplicationHistory.js
@@ -2,7 +2,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import JobApplicationRow from './JobApplicationRow';
-// import '../css/JobApplicationHistory.css';
+import '../css/JobApplicationHistory.css';
 
 const JobApplicationHistory = () => {
   const [jobApplications, setJobApplications] = useState([]);
