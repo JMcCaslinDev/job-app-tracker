f773ae735162c91df6609e3be4a42531b0172f9f
jmccaslin21 <80499963+JMcCaslinDev@users.noreply.github.com>
2024-05-07 13:19:07 -0700

    Removing auto add fields, building chrome ext

--- client/src/components/JobApplicationActions.js
+++ client/src/components/JobApplicationActions.js
@@ -4,21 +4,7 @@ import '../css/JobApplicationActions.css';
 
 const JobApplicationActions = ({ openModal }) => {
  
-  const handleAutoAdd = async () => {
-    const url = document.querySelector('.url-input').value;
-    const response = await fetch('/api/scrape-job-posting', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ url }),
-    });
-    const scrapedData = await response.json();
-    console.log("\nScraped Data: ", scrapedData, "\n");
-    // Use scrapedData to populate the form fields
-    // Example: document.querySelector('#company_name').value = scrapedData.companyName;
-    // ... populate other fields similarly
-  };
+ 
 
   const handleManualAdd = () => {
     openModal();
@@ -26,10 +12,6 @@ const JobApplicationActions = ({ openModal }) => {
 
   return (
     <div className="job-application-actions">
-      <div className="url-input-section">
-        <input type="text" className="url-input" placeholder="https://" />
-        <button onClick={handleAutoAdd}>Auto Add</button>
-      </div>
       <button onClick={handleManualAdd}>Manual Add</button>
     </div>
   );
